4aed99c4d64e9ed41b27f7f5970a1eaedf9f67bd
akshayparakh25 <sendtoakshayparekh@gmail.com>
2020-04-03 10:20:41 +0530

    updated tensorflow with version 1.15 in requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,6 @@
 virtualenv -p python3 env
 source env/bin/activate
-pip3 install jupyter ipython numpy matplotlib seaborn pandas tensorflow nltk
+pip3 install jupyter ipython numpy matplotlib seaborn pandas tensorflow==1.15 nltk
 pip install -U spacy
 python -m spacy download en_core_web_sm
 pip3 install torch==1.4.0+cpu torchvision==0.5.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
