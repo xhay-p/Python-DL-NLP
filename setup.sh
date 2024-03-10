ef706c65619cce1562395f366487cb9357859abd
akshayparakh25 <sendtoakshayparekh@gmail.com>
2024-03-10 21:15:48 +0530

    added langchain

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 virtualenv -p python3 env
 source env/bin/activate
+# For llama-cpp in Ubuntu
+sudo apt update
+sudo apt upgrade
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt update
+sudo apt install gcc-11 g++-11
 pip install -r requirements.txt
 python -m spacy download en_core_web_sm
